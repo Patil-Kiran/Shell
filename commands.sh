9af58f51579b5604690fba56ad0591e0e546f494
Patil-Kiran <82840059+Patil-Kiran@users.noreply.github.com>
2021-10-02 19:18:20 -0700

    Add files via upload

---
+++ commands.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+#1) ls - list all files in directory
+
+ls
+
+#2) list all files with hidden files in directory
+
+ls -la
+
+#3) current working directory 
+
+pwd 
+
+#4) retunring to previous directory
+
+cd ../../..
+
+#5) for root folder / and for home folder ~
+
+cd /
+pwd
+
+cd ~
+pwd
+
+
